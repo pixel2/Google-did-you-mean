b9acfb2acf024b9deecefdc006b552a2df1169c2
Emil Janitzek <emil@Emil-Janitzeks-MacBook.local>
2010-07-14 13:27:44 +0200

    Added the source code

---
+++ GoogleDidYouMean.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Author:    Emil Janitzek
+Autor URI: http://pixel2.se
+License:   You may redistribute it and/or modify it under the terms of the GNU 
+           General Public License as published by the Free Software Foundation, 
+           either version 3, (at your option) or any later version.
+*/
+class GoogleDidYouMean {
+  
+  private function curl_fetch($url) {
+    $options = array( 
+            CURLOPT_RETURNTRANSFER => true,     // return web page 
+            CURLOPT_HEADER         => false,     // return headers 
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects 
+            CURLOPT_ENCODING       => "uft-8",       // handle all encodings 
+            CURLOPT_USERAGENT      => "Mozilla/5.0 (Linux; U; Android 2.1-update1; en-gb; HTC Desire Build/ERE27) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17", // who am i 
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect 
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect 
+            CURLOPT_TIMEOUT        => 120,      // timeout on response 
+            CURLOPT_MAXREDIRS      => 4,        // stop after 4 redirects 
+            ); 
+
+    $ch      = curl_init( $url ); 
+    curl_setopt_array( $ch, $options ); 
+    $content = curl_exec( $ch ); 
+    $err     = curl_errno( $ch ); 
+    $errmsg  = curl_error( $ch ); 
+    $header  = curl_getinfo( $ch ); 
+    curl_close( $ch );
+    
+    return array($content,$header,$err,$errmsg);
+    
+  }
+  
+  public function doSpellingSuggestion($phrase, $lang = null) {
+    
+    if ($lang == "sv")
+      $url = "http://www.google.se/m?dc=gorganic&source=mobileproducts&q=";
+    else
+      $url = "http://www.google.com/m?dc=gorganic&source=mobileproducts&q=";
+    
+    list($content,$header,$err,$errmsg) = self::curl_fetch($url . $phrase);
+    
+    /* No website at this location */
+    if (!$content || $header['http_code'] != 200) {
+      return '';
+    }
+    
+    /* Match against the spell suggestion box and get the suggested word if exists */
+    $pattern = '/<div[^>]*spelling_onebox_result[^>]*>.*?<b><i>(.+?)<\/i><\/b>.*<\/div>/';
+    preg_match($pattern, $content, $matches);
+    
+    return $matches[1];
+    
+  }
+  
+}
+
+if (!empty($_GET["q"])) {
+  
+  $phrase   = trim($_GET["q"]);
+  $lang     = trim($_GET['hl']);
+  $dataType = trim($_GET['dataType']);
+  
+  $google = new GoogleDidYouMean();
+  
+  $result = $google->doSpellingSuggestion(urlencode($phrase), $lang);
+  
+  if ($dataType == "text") {
+    echo $result;
+  } else if(!empty($result)) {
+    echo json_encode(array('google' => array('suggestion' => $result)));
+  } else {
+    echo json_encode(array('google' => null));
+  }
+  
+}
+?>
---
+++ google_suggestion.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Google did-you-mean&trade;
+Plugin URI: http://blogg.pixel2.se
+Description:  Uses google did-you-mean&trade; to find misspellings and grammatical errors in searches queries, useful on search pages when no result was found. Include <code>google_suggestion();</code> in your code.
+Version: 1.0
+Author: Emil Janitzek
+Author URI: http://pixel2.se
+*/
+
+add_action( 'wp_print_scripts', 'init_jquery' );
+//add_action('get_search_query', 'proccess');
+
+function init_jquery() {
+	if (is_search()) wp_enqueue_script('jquery');
+}
+
+function google_suggestion($lang = null) {
+	
+	if ($lang == "sv")
+		$text = "Menade du:";
+	else
+		$text = "Did you mean:";
+
+	echo '<div class="gdym" style="display: none;"><span class="gdym-text" style="color:red;">'.  $text .'</span> <a href="#" class="gdym-result" style="font-weight: bold;"></a></div>';
+	
+	echo '<script type="text/javascript">(function($) {
+$.ajax({ url: "/wp-content/plugins/google-did-you-mean/GoogleDidYouMean.php", data: {"q": "'. $_GET['s'] .'", "hl": "'. (($lang==null)?'en':$lang) .'", "dataType": "text"}, success: function(data){
+if (data.length > 0) {
+$(".gdym-result").attr("href","?s="+ data).text(data);
+$(".gdym").show();
+}
+}}); })(jQuery);</script>';
+}
+
+?>
